2195a672322a785be7c8994899f14780bf501737
JonasKrueger99 <49757490+JonasKrueger99@users.noreply.github.com>
2019-04-29 10:39:25 +0200

    Update Jonas.py

--- Blatt3/Jonas.py
+++ Blatt3/Jonas.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 tmax = 3
 h = 0.01
 
-def f(p1, p2):
-    return [p1*(2-0.02*p2), -p2*(0.8-0.0002*p1)]
+def f(p1, p2, c = 1):
+    return [c*p1*(2-0.02*p2), -c*p2*(0.8-0.0002*p1)]
 
 def rk4(p1, p2, h):
     k = []
     k.append(f(p1, p2))
-    k.append(f(p1 + h/2*k[0][0], p2 + h/2*k[0][1]))
-    k.append(f(p1 + h/2*k[1][0], p2 + h/2*k[1][1]))
+    k.append(f(p1 + h/2*k[0][0], p2 + h/2*k[0][1], 2))
+    k.append(f(p1 + h/2*k[1][0], p2 + h/2*k[1][1], 2))
     k.append(f(p1 + h*k[2][0], p2 + h*k[2][1]))
     ks = np.add(np.add(np.add(k[0], k[1]), k[2]), k[3])
     return p1 + h/6*(ks[0]), p2 + h/6*(ks[1])
