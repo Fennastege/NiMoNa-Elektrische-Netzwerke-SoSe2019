dfd1c3f4dce9854e948ee894cdd0e7f40c923ed8
JonasKrueger99 <49757490+JonasKrueger99@users.noreply.github.com>
2019-04-29 13:09:55 +0200

    Update Jonas.py
    Jetzt sollte es endlich richtig sein :)

--- Blatt3/Jonas.py
+++ Blatt3/Jonas.py
@@ -1,20 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-tmax = 3
-h = 0.01
-
-def f(p1, p2, c = 1):
-    return [c*p1*(2-0.02*p2), -c*p2*(0.8-0.0002*p1)]
+def f(p1, p2):
+    return p1*(2-0.02*p2), -p2*(0.8-0.0002*p1)
 
 def rk4(p1, p2, h):
-    k = []
-    k.append(f(p1, p2))
-    k.append(f(p1 + h/2*k[0][0], p2 + h/2*k[0][1], 2))
-    k.append(f(p1 + h/2*k[1][0], p2 + h/2*k[1][1], 2))
-    k.append(f(p1 + h*k[2][0], p2 + h*k[2][1]))
-    ks = np.add(np.add(np.add(k[0], k[1]), k[2]), k[3])
-    return p1 + h/6*(ks[0]), p2 + h/6*(ks[1])
+    k1, l1 = f(p1, p2)
+    k2, l2 = f(p1 + h/2*k1, p2 + h/2*l1)
+    k3, l3 = f(p1 + h/2*k2, p2 + h/2*l2)
+    k4, l4 = f(p1 + h*k3, p2 + h*l3)
+    ks = k1 + 2*k2 + 2*k3 + k4
+    ls = l1 + 2*l2 + 2*l3 + l4
+    return p1 + h/6*(ks), p2 + h/6*(ls)
 
 def rb(tmax, h, p10, p20):
     p1 = [0]*(int(tmax/h))
@@ -29,10 +26,12 @@ def rb(tmax, h, p10, p20):
         if (mess + 10 < pro):
             print(str(round(pro, 0))+"% Fortschritt")
             mess = pro
-    #plt.plot(np.arange(0, tmax, h), p1)
-    #plt.plot(np.arange(0, tmax, h), p2)
-    plt.plot(np.arange(0, tmax, h), np.subtract(p1, p2))
-    #plt.plot(p1, p2)
+    plt.plot(np.arange(0, tmax, h), p1, label = "p1")
+    plt.plot(np.arange(0, tmax, h), p2, label = "p2")
+    plt.plot(np.arange(0, tmax, h), np.subtract(p1, p2), label = "p1-p2")
+    plt.legend()
+    plt.show()
+    plt.plot(p1, p2)
     plt.show()
 
-rb(200, 0.0005, 500, 400)
+rb(500, 0.025, 1000, 20)
